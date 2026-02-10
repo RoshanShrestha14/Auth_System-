e4a23bcb6447f69112dba881aa7e0e2adc226e93
Roshan Shrestha <roshanshresthawd@gmail.com>
2026-02-10 13:29:10 +0545

    implemented Validation Pipe

--- src/auth/dto/register.User.ts
+++ src/auth/dto/register.User.ts
@@ -1,6 +1,19 @@
+import { IsEmail, IsString, IsNotEmpty } from 'class-validator';
+
 export class registerUserDto {
+  @IsString()
+  @IsNotEmpty()
   fname: string;
+
+  @IsString()
+  @IsNotEmpty()
   lname: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
